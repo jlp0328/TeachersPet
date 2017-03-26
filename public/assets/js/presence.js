ddd9dcf6af52b78521dffde6d06d686e99893704
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2017-03-26 19:23:20 -0400

    changed part of classPresence to grab proper info, only shows info for first student

--- public/assets/js/presence.js
+++ public/assets/js/presence.js
@@ -1,41 +1,43 @@
 
 $(function(){
 
-
 var presenceStatus = $("select");
-console.log(presenceStatus);
 
-var classPresence = [];
-var date = $("#date");
-      console.log(date);
 
 var subject = $("#subjectId").val();
       console.log(subject);
+
+var classPresence = [];
+
 
 // var student_id = presenceStatus.attr("data-studentId");
 //      console.log(student_id);
 
 $(".submitButton").on("click", function(){
 
-    alert("Working!");
+var date = $("#date").val().trim();
+      console.log(date);
 
-    presenceStatus.each(function(i,presence){
+console.log(presenceStatus.val());
+
+    $(".student").each(function(i,presence){
 
     var student_id = presenceStatus.attr("data-studentId");
           console.log(student_id);
 
             classPresence.push({
-              "student_id": this.student_id,
-              "date": this.date,
-              "subject":this.subject,
+              "student_id": student_id,
+              "date": date,
+              "subject":subject,
               "presence": presenceStatus.val()
           });
+          console.log(classPresence[i]);
       });
-          console.log(classPresence);
+    // alert("Working!");
 
 });
 
-    $.post("/attendance", classPresence, function(data) {});
+    // $.post("/attendance", classPresence, function(data) {});
 
 });
 
